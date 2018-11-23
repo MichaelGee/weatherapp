3d659afe504191320c4f7753bc17895e72fb14bb
MichaelGee <michaelgatesjr@gmail.com>
2018-11-23 22:05:38 +0100

    Updates

---
+++ precache-manifest.f94fb848e31b7fb493d0cb2a2030d5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7ea501e7b8f22ce0b7",
+    "url": "/weatherapp/static/js/runtime~main.0e7ea501.js"
+  },
+  {
+    "revision": "d02b65067b3de0320bfc",
+    "url": "/weatherapp/static/js/main.d02b6506.chunk.js"
+  },
+  {
+    "revision": "00aaafe26d0b80d234fd",
+    "url": "/weatherapp/static/js/1.00aaafe2.chunk.js"
+  },
+  {
+    "revision": "d02b65067b3de0320bfc",
+    "url": "/weatherapp/static/css/main.586ebfb9.chunk.css"
+  },
+  {
+    "revision": "00aaafe26d0b80d234fd",
+    "url": "/weatherapp/static/css/1.b9af28e3.chunk.css"
+  },
+  {
+    "revision": "155da8fd8131171a8f4aea2b0d42ce31",
+    "url": "/weatherapp/index.html"
+  }
+];
