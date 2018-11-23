769ebbbea6c96a3eeaf07c56b71fce93d1b583c8
MichaelGee <michaelgatesjr@gmail.com>
2018-11-23 14:24:38 +0100

    Change to some attractive colors

--- src/components/form.js
+++ src/components/form.js
@@ -5,7 +5,7 @@ import React from 'react'
    <div className="row align-items-center justify-content-center">
    <div className=" col-10 col-md-6 col-sm-8">
     <form onSubmit={props.getWeather}>
-    <h2 className="text-center">Add a city and country</h2>
+    <h2 className="text-center subtext">Add a city and country</h2>
     <input type="text" class="form-control mb-2" name='city' placeholder="City"/>
     <input type="text" class="form-control mb-2" name='country' placeholder="Country"/>
     <button className="btn btn-primary align-items-right">Submit</button>
--- src/components/title.js
+++ src/components/title.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Title =()=>(
-  <div className="text-center">
+  <div className="text-center title">
   <h1>WeatherLens</h1>
   </div>
 );
